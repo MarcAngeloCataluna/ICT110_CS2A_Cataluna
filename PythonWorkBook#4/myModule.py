5e89396efdefe0d097e5d1c387b0dff8d7dc9a35
MarcAngeloCataluna <marcangelo.cataluna@students.isatu.edu.ph>
2024-04-12 19:00:38 +0800

    PythonModules Update
    + Answered Exercise 1 and 2

---
+++ PythonWorkBook#4/myModule.py
@@ -0,0 +1,19 @@
+
+"""
+Marc's Module
+(probably useless)
+https://www.youtube.com/watch?v=dQw4w9WgXcQ
+"""
+
+magic_word = "i need more bullets"
+
+
+def arrivalTime(distance, speed):
+    """
+    Parameter:
+        distance in km
+        speed in km/s
+    Returns: estimated arrival time in hour
+    """
+    time = float(distance) / float(speed)
+    return(time)
